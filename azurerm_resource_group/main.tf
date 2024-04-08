34b46e27a036179417c880e84c0ffe6b5e339ba1
vara prasad reddy jaggu <prasad.reddy0708@gmail.com>
2024-04-08 18:29:46 +0530

    added azure resource group code

---
+++ azurerm_resource_group/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.37.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+
+  }
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.location
+}
