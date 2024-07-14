0918ffc71122ee27b8c863e7af40dd4d5301a1c9
vara prasad reddy jaggu <prasad.reddy0708@gmail.com>
2024-07-14 19:00:15 +0530

    added logic for conditional resources

--- terraform-for-each-example/main.tf
+++ terraform-for-each-example/main.tf
@@ -37,7 +37,7 @@ output "resources_map_output" {
 }
 
 resource "null_resource" "resources_list_of_objects" {
-  for_each = { for each in var.resources_list_of_objects : "${each.deployment_id}" => each }
+  for_each = { for resource in var.resources_list_of_objects : "${resource.deployment_name}" => resource }
   triggers = {
     name = each.key
   }
@@ -45,4 +45,43 @@ resource "null_resource" "resources_list_of_objects" {
 
 output "resources_list_of_objects_output" {
   value = null_resource.resources_list_of_objects
+}
+
+//conditionally create resources using for_each with list_of_objects
+
+resource "null_resource" "conditional_resources_list_of_objects" {
+  for_each = { for resource in var.resources_list_of_objects : "${resource.deployment_name}" => resource if resource.create_resource }
+  triggers = {
+    name = each.key
+  }
+}
+
+output "conditional_resources_list_of_objects_output" {
+  value = null_resource.conditional_resources_list_of_objects
+}
+
+
+//create multiple resources using for_each with map_of_objects
+resource "null_resource" "resources_map_of_objects" {
+  for_each = var.resources_map_of_objects
+  triggers = {
+    name = each.key
+  }
+}
+
+output "resources_map_of_objects_output" {
+  value = null_resource.resources_map_of_objects
+}
+
+//conditionally create resources using for_each with map_of_objects
+
+resource "null_resource" "conditional_resources_map_of_objects" {
+  for_each = { for resourceKey, resourcevalue in var.resources_map_of_objects : resourceKey => resourcevalue if resourcevalue.create_resource }
+  triggers = {
+    name = each.key
+  }
+}
+
+output "conditional_resources_map_of_objects_output" {
+  value = null_resource.conditional_resources_map_of_objects
 }
--- terraform-for-each-example/variables.tf
+++ terraform-for-each-example/variables.tf
@@ -1,48 +1,69 @@
 variable "resources_list" {
   type    = list(string)
-  default = ["resource1", "resource2", "resource3"]
+  default = ["ubuntu-vm", "windows-vm", "macos-vm"]
 }
 
 variable "resources_map" {
   type = map(string)
   default = {
-    "resource1" = "Tom",
-    "resource2" = "Mike",
-    "resource3" = "Jerry",
+    "resource1" = "ubuntu-vm",
+    "resource2" = "windows-vm",
+    "resource3" = "macos-vm",
   }
 }
 
 variable "resources_list_of_objects" {
   type = list(object({
-    deployment_id = string
-    model_name    = string
-    model_format  = string
+    deployment_name = string
+    model_name      = string
+    model_format    = string
+    model_version   = string
+    create_resource = bool
   }))
   default = [{
-    deployment_id = "gpt-3"
-    model_name    = "gpt-3"
-    model_format  = "OpenAI"
+    deployment_name = "gpt35turbo16k"
+    model_name      = "gpt-35-turbo-16k"
+    model_version   = "0613"
+    model_format    = "OpenAI"
+    create_resource = true
     },
-    { deployment_id = "gpt-4"
-      model_name    = "gpt-4"
-      model_format  = "OpenAI"
+    {
+      deployment_name = "gpt35turbo"
+      model_name      = "gpt-35-turbo"
+      model_version   = "0301"
+      model_format    = "OpenAI"
+      create_resource = false
+    },
+    {
+      deployment_name = "gpt4"
+      model_name      = "gpt-4"
+      model_version   = "0314"
+      model_format    = "OpenAI"
+      create_resource = true
   }]
 }
 
-variable "resources_list_of_objects2" {
+variable "resources_map_of_objects" {
   type = map(object({
-    deployment_id = string
-    model_name    = string
-    model_format  = string
+    deployment_name = string
+    model_name      = string
+    model_format    = string
+    model_version   = string
+    create_resource = bool
   }))
   default = {
     "deployment_1" = {
-      deployment_id = "gpt-3"
-      model_name    = "gpt-3"
-      model_format  = "OpenAI"
+      deployment_name = "gpt35turbo16k"
+      model_name      = "gpt-35-turbo-16k"
+      model_format    = "OpenAI"
+      model_version   = "0613"
+      create_resource = true
     },
-    "deployment_2" = { deployment_id = "gpt-4"
-      model_name   = "gpt-4"
-      model_format = "OpenAI"
+    "deployment_2" = {
+      deployment_name = "gpt4"
+      model_name      = "gpt-4"
+      model_format    = "OpenAI"
+      model_version   = "0314"
+      create_resource = false
   } }
 }
